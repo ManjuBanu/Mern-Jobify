d36493dbd1e734b68708388e3b46a94e2add30f2
MANJU MALA DEVI M <manjum23@in.ibm.com>
2023-02-23 00:02:43 +0530

    pstman-setup

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from 'express';
-import connectDB from './db/connect.js';
-
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import notFoundMiddleware from './middleware/not-found.js';
-import dotenv from 'dotenv';
-
 const app = express();
-
+import dotenv from 'dotenv';
 dotenv.config()
 
+// db and authenticateUser
+import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
+
+// middleware
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
+app.use(express.json());
+
 app.get('/',(req,res) =>{
-    throw new Error('error');
+    // throw new Error('error');
     res.send('welcome!')
 })
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -25,14 +36,14 @@ const port = process.env.PORT || 5000;
 
 // mongoose connect method returns a promise therefore in server setup async
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URL);
-      app.listen(port, () => {
-        console.log(`Server is listening on port ${port}...`);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  start();
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
